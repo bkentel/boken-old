54e32ea96fa88cb24535e40633d249204d8c64d4
Brandon Kentel <bkentel@users.noreply.github.com>
2016-04-30 21:35:11 -0400

    add "identified" property for items

--- src/item_properties.hpp
+++ src/item_properties.hpp
@@ -6,15 +6,21 @@
 namespace boken {
 
 //! common item properties
-enum class item_property : item_property_id::type {
+enum class item_property : uint32_t {
     weight             = djb2_hash_32c("weight")
   , capacity           = djb2_hash_32c("capacity")
   , stack_size         = djb2_hash_32c("stack_size")
   , current_stack_size = djb2_hash_32c("current_stack_size")
+  , identified         = djb2_hash_32c("identified")
 };
 
+//! item_property:identified
+//! 0 -- unknown
+//! 1 -- seen
+
 inline constexpr item_property_id property(item_property const p) noexcept {
-    return item_property_id {static_cast<item_property_id::type>(p)};
+    using type = std::underlying_type_t<item_property>;
+    return item_property_id {static_cast<type>(p)};
 }
 
 } // namespace boken
